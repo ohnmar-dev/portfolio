05d9c3b7daa5cf97bb998af881234197654cb0bf
Kyaw Soe Hla <codewarrior.ksh@gmail.com>
2024-11-13 12:14:28 -0800

    add gitignore

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Album, Gallery
+# Register your models here.
+
+admin.site.register(Album)
+admin.site.register(Gallery)
