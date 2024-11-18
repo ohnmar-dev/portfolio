160ccbf3fa43bc15c6ed6f4768fb47da93624e81
Kyaw Soe Hla <codewarrior.ksh@gmail.com>
2024-11-18 12:16:00 -0800

    Retrieve Datat form blog table

--- base/views.py
+++ base/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
-from .models import Gallery, Album
+from .models import Gallery, Album, Category, Blog
 
 # Create your views here.
 def index(request):
     albums = Album.objects.all()
     photos = Gallery.objects.all()
+    blogs = Blog.objects.all()
     context = {
         'albums':albums,
         'photos':photos,
+        'blogs':blogs,
     }
     return render(request, 'index.html',context)
